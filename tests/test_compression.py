8a15e3af0d40b15ca617aa007c882b461f7a83de
TheHardew <7374061+TheHardew@users.noreply.github.com>
2024-01-31 12:57:43 +0100

    add tests fixes #11 maybe other tests should be added, not sure which for now

---
+++ tests/test_compression.py
@@ -0,0 +1,29 @@
+import compress_comics_TheHardew
+from tempfile import TemporaryDirectory
+import sys
+from pathlib import Path
+import zipfile
+import os
+
+def get_zipfile_files(filepath):
+    with zipfile.ZipFile(filepath, 'r') as zipf:
+        return sorted(zipf.namelist())
+
+
+# TODO maybe don't rely on the expected_files txt file
+def test():
+    with open('expected_files.txt', 'r') as file:
+        expected_files =  sorted([line.strip() for line in file])
+
+    with TemporaryDirectory() as tmpd:
+        sys.argv = ['compress_comics', '-e1', tmpd]
+        compress_comics_TheHardew.main()
+
+        for file in Path.cwd().rglob('*'):
+            if file.is_file() and file.suffix.lower() in ['.cbz', '.cbr']:
+                file = file.with_suffix('.cbz')
+                files = get_zipfile_files(Path(tmpd) / file.name)
+                assert files == expected_files
+
+if __name__ == "__main__":
+    test()
